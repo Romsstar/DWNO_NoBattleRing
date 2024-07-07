1cc379cd632efb2771c06a15d2d68f8e7791fdb3
Romsstar <romsstar@gmail.com>
2024-07-07 18:23:47 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+
+namespace NoBatttleRing;
+
+[BepInPlugin(GUID, PluginName, PluginVersion)]
+[BepInProcess("Digimon World Next Order.exe")]
+public class Plugin : BasePlugin
+{
+    internal const string GUID = "Romsstar.DWNO.NoRing";
+    internal const string PluginName = "NoBattleRing";
+    internal const string PluginVersion = "1.0";
+
+    public override void Load()
+    {
+        Awake();
+    }
+
+    public void Awake()
+    {
+        Harmony harmony = new Harmony("Patches");
+        harmony.PatchAll();
+    }
+
+
+
+    [HarmonyPatch(typeof(MainGameManager), "Start")]
+    public static class MainGameManagerPatch
+    {
+
+        [HarmonyPatch(typeof(MainGameManager), "Start")]
+        public static bool Prefix(MainGameManager __instance)
+        {
+            // Disable the battleArea projector by setting it to null before the method executes
+            __instance.m_battleArea = null;
+            return true;
+        }
+    }
+}
+        
---
+++ README.md
@@ -0,0 +1,12 @@
+# DWNO No Battle Ring
+This mod removes the red boundary Ring in battles.
+Untested and might not work everywhere.
+
+# Installation
+1. Install BepInEx (Tutorial by Yasha-jin)
+2. Download the latest release from the Releases Tab
+3. Copy the NoBattleRing.dll into the plugins folder of BepinEx
+4. Start the game and enjoy :)
+
+# Contact
+Discord: Romsstar#8098, or in either the Digimon Modding Community or Digimon Discord Community
